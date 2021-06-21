a4e101418e8f808511c855d14c9dd242b79a567f
rawan ahmad fawzi khasawneh <rawanahmed115@gmail.com>
2021-06-21 21:38:30 +0300

    front end

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
+import BeastBooks from './components/BeastBooks';
 
 export class Profile extends Component {
     constructor(props) {
@@ -7,16 +9,41 @@ export class Profile extends Component {
         this.state = {
             userName: this.props.auth0.user.name,
             userEmail: this.props.auth0.user.email,
-            userPicture: this.props.auth0.user.picture
+            userPicture: this.props.auth0.user.picture,
+            serverUrl: process.env.REACT_APP_SERVER_URL,
+            booksData: []
         }
     }
+
+    componentDidMount = async () => {
+        await axios.get(`${this.state.serverUrl}/books?email=${this.state.userEmail}`).then(response => {
+            this.setState({
+                booksData: response.data[0].books
+            })
+        }).catch(
+            error => {
+                alert(error.message);
+            }
+        );
+    }
+
     render() {
         return (
+            <div>
             <div>
                 <h2>{this.state.userName}</h2>
                 <p>{this.state.userEmail}</p>
                 <img src={this.state.userPicture} alt={this.state.userName} />
             </div>
+            {this.state.booksData.length > 0 &&
+                <div>
+                    <BeastBooks
+                        books={this.state.booksData}
+                    />
+                </div>
+            }
+            </div>
+
         )
     }
 }
---
+++ src/components/BeastBooks.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class BeastBooks extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>My Favourite Books</h2>
+                {this.props.books.length && this.props.books.map((book, idx) => (
+                    <>
+                    <div key={idx}>
+                        {book.name}
+                    </div>
+                    <div key={idx}>
+                        {book.description}
+                    </div>
+                    <div key={idx}>
+                        {book.status}
+                    </div>
+                    </>
+                ))}
+            </>
+        )
+    }
+}
+
+export default BeastBooks;
