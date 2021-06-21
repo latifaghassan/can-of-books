784cfe951c9f1b063b8b37dade793a53b025b1a0
rawan ahmad fawzi khasawneh <rawanahmed115@gmail.com>
2021-06-21 15:31:57 +0300

    create a auth0 app

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+import './Header.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import Logout from './components/Logout';
+import LoginButton from './components/LoginButton';
+
+class Header extends React.Component {
+  render() {
+    const { isAuthenticated } = this.props.auth0;
+    return (
+      <header className="jumbotron text-center">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>My Favorite Books</Navbar.Brand>
+          <Link to="/">Home</Link>
+          <Link to="/Profile">Profile</Link>
+          {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+          {
+            isAuthenticated ? <Logout /> : <LoginButton />
+          }
+
+        </Navbar>
+      </header>
+
+    )
+  }
+}
+
+export default withAuth0(Header);
